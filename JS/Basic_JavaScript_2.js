70c27590f1a5f4b17a21d01faf8d5cc1e6f4366a
PinkBeeMe <51229658+PinkBeeMe@users.noreply.github.com>
2020-01-08 18:53:31 -0600

    javascript project work with functions

---
+++ JS/Basic_JavaScript_2.js
@@ -0,0 +1,22 @@
+function changeColor(newColor) {
+    var elem = document.getElementById('para');
+    elem.style.color = newColor;
+}
+
+var x = document.getElementById("myAudio") , y = document.getElementById("myAudio");
+function playAudio() {
+    x.play();
+}
+ function pauseAudio() {
+     y.pause();
+ }
+ 
+ var xx = document.getElementById("myAudio2"), yy = documnet.getElementById("myAudio2");
+ function playAudio() {
+    xx.play();
+}
+ function pauseAudio() {
+     yy.pause();
+ }
+
+
