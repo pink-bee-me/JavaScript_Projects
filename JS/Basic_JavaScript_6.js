52d67fd4214a0a741e23641f02793bb824321e80
LaurieSue <pink.bee.me@outlook.com>
2020-01-29 01:46:30 -0600

    resubmit exercises

--- JS/Basic_JavaScript_6.js
+++ JS/Basic_JavaScript_6.js
@@ -1,25 +1,65 @@
-function Ride_Function()  {
-    var Height, Can_Ride;
-    Height = document.getElementById("Height").value;
-    Can_Ride = (Height < 52) ? "You are too short":"you are tall enough";
-    document.getElementById("Ride").innerHTML = Can_Ride + " to ride.";
+// Step 120-121 JavaScript Exercise //
+function Vehicle(Make, Model, Year, Color) {
+        this.Vehicle_Make = Make;
+        this.Vehicle_Model = Model;
+        this.Vehicle_Year = Year;
+        this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily =  new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+    document.getElementById("Keywords_and_Constructors").innerHTML = "Erik drives a "  + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
 }
 
-//Age Verification for Cast Your Vote Program //
-function  R_U_Old_E_Nuff()  {
- var d = new Date()
- 
 
 
+
+
+
+
+// Step 124 Reserved Words and Object Constructor Function  JavScript Exercises 
+// var true;
+//true = true;
+//function  myReservedWord() {
+//document.getElementById("Reserved_Word").innerHTML = "1 + 1 = 2 " + true;
+//}
+//console.log(true);
+//THIS DOESN'T WORK AND SHUTS DOWN MY WHOLE PAGE!!!! //
+
+
+// Step 122 New and This Keywords and Step 124 Object Constructor -  Javascript Exercises //
+function Journal_Log(Entry_Date, Department, Entry, Created_By) {
+    this.Journal_Log_Entry_Date = Entry_Date;
+    this.Journal_Log_Department = Department;
+    this.Journal_Log_Entry = Entry;
+    this.Journal_Log_Created_By = Created_By;
 }
 
-function age_calculation() {
- var diff_ms = Date.now() - dob.getTime()
- var age_dt = new Date(diff_ms);
+var entry1 = new Journal_Log("01/19/2020", "Accounting", "Checked Out File# 16752", "LaurieSue Peterson");
+var entry2 = new Journal_Log("01/19/2020", "Purchasing", "Checked Out File# 39972", "Kanye West");
+var entry3 = new Journal_Log("01/17/2020", "Sales", "Checked Out File# 75534", "Elmer Fudd");
+var entry4 = new Journal_Log("01/10/2020", "Security", "Checked Out File# 446289", "Yosemite Sam");
 
- return Math.abs(age_dt.getUTCFullYear() - 1970);
+function my_New_This()  {
+    document.getElementById("New_and_This").innerHTML =
+    "On " + entry4.Journal_Log_Entry_Date + " The Journal/Log Entry Was: " +
+     entry4.Journal_Log_Entry + "  " + entry4.Journal_Log_Department + "  " + entry4.Journal_Log_Created_By;
 }
 
-console.log(caluclate_age(new Date(1982,11,4)));
 
 
+
+
+// Step 126-127 Nested Functions - JavaScript Exercises //
+function Nested_Function() {
+    document.getElementById("Nested_Function").innerHTML = count();
+    function count() {
+        var start = 0;
+        function plus_ten(){
+            start += 10;
+        }
+    plus_ten();
+    return start;
+    }
+}
---
+++ JS/myFunction.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    document.getElementById("Keywords_and_Constructors").innerHTML = "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
+}
