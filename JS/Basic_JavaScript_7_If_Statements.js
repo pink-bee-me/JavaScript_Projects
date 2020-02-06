ed746ed96aa1fcc09045a22219c7ffc4b9eb5e17
LaurieSue <pink.bee.me@outlook.com>
2020-02-05 19:13:08 -0600

    was working added greeting function and now undefined function / added greeting script file

--- JS/Basic_JavaScript_7_If_Statements.js
+++ JS/Basic_JavaScript_7_If_Statements.js
@@ -3,110 +3,90 @@
    // JavaScript Exercises //
 
 
-    //Declare Global Variables to be used in amEyeLate() **imagine that this is part of an optometrist office management system...this function being used to handle the scheduling procedure for patient's yearly eye Exams . The purpose is to implement a standard operating procedure for the office staff to follow to improve the patient adherence to the yearly Eye Exam Recommendation.//
+    //Declare Global Variables to be used in amEyeLate() **imagine that this is part of an optometrist office management system...this function being used to handle the scheduling procedure for patient's yearly eye Exams . The purpose is to implement a standard operating procedure for the office staff to follow to improve patient adherence to the yearly Eye Exam Recommendation.//
 
 
     // assume that another part of the office management system would provide the input for these variables based on the date of the patient's last eye exam.//
-   
-    let patient = "Martha Saunders";
-   
-    let year = 2019;
-    let month = 03;
-    let day = 03;
+ function amEyeLate()  {
+//Patient Name//
+    var patient = "Martha Saunders";
 
-today =  new Date();
-   
-lastEyeExam = new Date(year,month,day);
-lastEyeExam.setDate(year,month,day);
+ // feel free to change the variables to test the demo ... these are made-up (assumed) variables that  represent the office data that would be  passed from data stored with in another section of the office management and records keeping software already installed at the location of the demo trial//  
+    var year = 2019;
+    var month = 02;
+    var day = 03;
+//Current date and Time --Today's date//
+var currentDate =  new Date();
 
-nextEyeExam = new Date();
-nextEyeExam.setDate(year,(month + 11),day);
+// Date Object  Last Eye Exam created with the new Date() constructor is passed the variables: year, month, and day ... (lines 32,33,34 respectively(above))  hypothgetically from the office records database based on the last eye exam that the patient has on record with the clinic.. This is the date that all subsequent figures and dates are either constructed from or referenced to in someway or another.// 
+var lastEyeExam = new Date(year,month,day);
+console.log(lastEyeExam + "  lastEyeExam"); // check value in console  
 
-mailOutReminder = new Date();
-mailOutReminder.setDate((+ 10);
-    let actionDate = today;
-    let followUpDate = today.setDate(+ 14);
-
-    let yearlyEyeExamScheduled = "Scheduled Appointment Will Be Recorded Here";
+//nextEyeExam variable - created via the passing of the lastEyeExam value to  the new Date() constructor; once the value is passed to nextEyeExam, the .setDate() and .getDate() methods are used to set the date forward a year ( value I chose to use was 365 days, because my measuure did not need to be precise, close in gerneral is good enough as long as teh unit of time is measurable and consistant //
+var nextEyeExam = new Date(lastEyeExam);
+newEyeExam.setDate(newEyeExam.getDate() + 365 );
+console.log(nextEyeExam + "  nextEyeExamDue"); // check value in console
 
 
-    function amEyeLate()  {
-         if (((nextEyeExam.getMonth()) >= (lastEyeExam.getMonth(+10))) {
-    document.getElementById("Patient_History_Status").innerHTML= "Patient_History_Status_Information: [ Today's Date: " + today + " ]     [ Patient: " +  patient + " ]   [ Date of Last Eye Exam: " + lastEyeExam + " ]    [ Date Patient is Due for Next Yearly Eye Exam: " + nextEyeExam + " ]"; 
-         }
-   
-    }
- console.log(patient);
-    console.log(lastEyeExam);
-    console.log(nextEyeExam);
-    console.log(patient,lastEyeExam,nextEyeExam);
+// mailOutReminder  represents the date  a reminder should be mailed to glean optimal results: created by passing the nextEyeExam Date value to the new Date() constructor: the variable once initialized is further defined with the setDate() and getDate()method: the mailOutReminder date is 30 days prior to the nextEyeExam date, which gives the office 30 days to contact the patient, give patient time to respond (14 days) ,reach out to patient through follow-up call and  schedule the eye exam.
+var mailOutReminder = new Date(nextEyeExam);
+mailOutReminder.setDate(mailOutReminder.getDate() - 30);
+ console.log(mailOutReminder +  "  mailOutReminder");
 
 
+ //Scenario: The current date is a time period longer than 30 days ( which is the suggested length of time between the mailout date and the next Eye Exam)...This section is here to calculate the difference(in days) between the current date and the pre-figured mailOutReminder date. Then, that diff value will be  used  to offset the mailout date to the current date , and teh followup date 14 days after that date. The exam date will not change. when scheduling the appointment for the Exam the last exam date which will be this years exam date will effectively get the cycle back up to date. The goal is to establish the  correct date that the reminder will be mailed ; therefore effectively ensuring the appropriate time frame is still intact for the follow-up call and so forth. It essentially gets any lag in productivity or schedule gliches back in line with the program and the office schedule.Especially  useful, when starting up and working to process a new patient  data base. //
 
 
+ //Time Difference between currentDate and The MailOut Reminder Date used to determine the amount of offset (if any) is needed for accurate calculation of the datre to mail the Reminder and the follow-up call etc. Getting the Time value in milliseconds to be able to execute the subtraction operation//
+ var timeDiff = Math.abs(currentDate.getTime()) - mailOutReminder.getTime();
+
+ //Days Difference returning the value to "days" and determining the number of days difference between the current Date and the mail out Reminder date as figured previously.//
+  var daysDiff =  Math.ceil(timeDiff / (1000 * 3600 * 24));
+  console.log(daysDiff + "  days diff");
+
+ // This serves to correct any time lapse or descrepencies that may occur for various reasons, for example weekends,  or times when you cannot make calls that day, or even when you first implement the system and you may have to "catch-up" as far as your scheduling efforts may be out of sync and need an overhaul to get back on track...especially if efforts have been neglected in this particuular area for sometime. You may be dealing with a backlog of patients that have way over-due exams. This will get the mailout dates etc., back in line and accurate The creation of the reminderDate variable holds the value including any corrections that need to be handled.//
+var reminderDate = new Date(mailOutReminder);
+reminderMailedDate.setDate(reminderMailedDate.getDate() + daysDiff);
+console.log(reminderMailedDate + "  reminderMailedDate");
 
 
-
-
-    // function amEyeLate() {
-
-
-
-
-
-
+ var  followUpDate = new Date(mailOutReminder);
+followUpDate.setDate(followUpDate.getDate()+ 14);
+ console.log(followUpDate + "  followUpDate");
 
 
 
-    // }
+ 
 
 
 
 
+  
 
+         if   ((currentDate.getTime()) > (mailOutReminder.getTime()));
+    document.getElementById("Patient_History_Status").innerHTML= "Patient History  as of  Today's Date: " + currentDate;
+      document.getElementById("Patient_Name").innerHTML= "Patient: " +  patient ;
+       document.getElementById("Date_of_Last_Exam").innerHTML= "Date of Last Eye Exam: " + lastEyeExam;
+        document.getElementById("Next_Yearly_Eye_Exam").innerHTML= "Next Yearly Eye Exam Due: " +  nextEyeExam;
+      
+          document.getElementById("Reminder").innerHTML= "Date To Mail Reminder: " +  mailOutReminder;
+    
 
-
-
-
-
-
-
-
-
-
-    //}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if  (daysDiff > 0) ; 
+ document.getElementById("Not_Time_For_Reminder").innerHTML =  
+ "Send Out Reminder - Past Due as of : " + mailOutReminder ;
+ if ((mailOutReminder.getTime())> (currentDate.getTime())); document.getElementById("Not_Time_For_Reminder").innerHTML ="Not Time For Reminder!";
+document.getElementById("Mail_Reminder").innerHTML = "Mail Reminder : " + mailOutReminder;
+document.getElementById("Follow_Up").innerHTML = "Follow Up with Telephone Call  If No Response From Patient By : " + followUpDate;
+document.getElementById("Message").innerHTML = "Assist Patient:  Schedule Appointment For Yearly Eye Exam Wellness Check";
+ 
 
 
 
 
     // if(((year) > (lastYear)) || (( month -10 ) >= (lastMonth))) {
-    // document.getElementById("EyeExam").innerHTML = "Action Date:" + mailOut + " Action: Mail Yearly Eye Exam Reminder including 15% discount. Follow Up Date: " + (today.getDate()+14)+ " Follow Up Action: Call patient to schedule exam if no RSVP to Mail-Out.";//
-    // }
-    //}
-    //console.log(lastExam)//
-    // }
+    // document.getElementById("EyeExam").innerHTML = "Action Date:" +////mailOut + " Action: Mail Yearly Eye Exam Reminder including 15% ////discount. Follow Up Date: " + (today.getDate()+14)+ " Follow Up //
+//Action: Call patient to schedule exam if no RSVP to Mail-Out.//
+
+console.log(reminderMailedDate + "  reminderMailOutDate");
+         }
