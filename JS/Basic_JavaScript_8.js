f80dd145d7154e29013aa1cc64f6472ff2d32e4f
LaurieSue <pink.bee.me@outlook.com>
2020-02-13 17:16:59 -0600

    issues with concatenate file Basic_HTML_8.html and Basic_JavaScript_8.js error undefined function

---
+++ JS/Basic_JavaScript_8.js
@@ -0,0 +1,12 @@
+function little_Piggies() {
+ var piggy_1 = "This Little Piggy by Mother Goose: ";   
+ var piggy_2 = "This little piggy went to market, ";
+ var piggy_3 = "This little piggy stayed home, ";
+ var piggy_4 = "This little piggy had roast beef, ";
+ var piggy_5 = "This little piggy had none. ";
+ var piggy_6 = "This little piggy went ... ";
+ var piggy_6 = "Wee, wee, wee,";
+ var piggy_7 = "all the way home! ";
+ var nursery_Rhyme = piggy_1.concat(piggy_2, piggy_3, piggy_4, piggy_5, piggy_6, piggy_7, piggy_8);
+ document.getElementById("Concatenate").innerHTML = nursery_Rhyme;
+ }
