d9b161e25e90ad521a4ad47d2c8cc98063c76096
LaurieSue <pink.bee.me@outlook.com>
2020-02-13 22:52:17 -0600

    Basic_HTML_8.html / Basic JavaScript_8.js  Question-Error UncaughtPromise...userId...anonymous something--What???

--- JS/Basic_JavaScript_8.js
+++ JS/Basic_JavaScript_8.js
@@ -5,8 +5,18 @@ function little_Piggies() {
  var piggy_4 = "This little piggy had roast beef, ";
  var piggy_5 = "This little piggy had none. ";
  var piggy_6 = "This little piggy went ... ";
- var piggy_6 = "Wee, wee, wee,";
- var piggy_7 = "all the way home! ";
+ var piggy_7 = "Wee, wee, wee,";
+ var piggy_8 = "all the way home! ";
  var nursery_Rhyme = piggy_1.concat(piggy_2, piggy_3, piggy_4, piggy_5, piggy_6, piggy_7, piggy_8);
  document.getElementById("Concatenate").innerHTML = nursery_Rhyme;
  }
+
+ function slice_Method() {
+    var sentence = "Mother Goose does not refer to one particular person; Instead, Charles Perrault (one of the initiators of the literary fairy tale genre)'s publication, Tales of My Mother Goose, which is the first authenticated starting-point for the Mother Goose stories.";
+    var sliced_piece = sentence.slice(55,15);
+    var long = sliced_piece
+    document.getElementById("Slice").innerHTML = long;
+    console.log(sliced_piece);
+    document.getElementById("Length").innerHTML = long.length;
+
+}
