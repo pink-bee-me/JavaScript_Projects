1a72bcf30dd45f3abd34989ec5608ca23a32ac57
LaurieSue <pink.bee.me@outlook.com>
2020-02-20 04:40:10 -0600

    question...why are 1 thru 3 potatoes NOT Printing to the window?

--- JS/Basic_JavaScript_10.js
+++ JS/Basic_JavaScript_10.js
@@ -1,9 +1,12 @@
-function Call_Loop() {
-var count = 1;
-var counting= "";
-while (counting < 21) {
-    counting += "<br>" + count;
-    (count++);
-}
-document.getElementById("Loop").innerHTML = counting;
+function Call_Why() {
+    let counter = 1;
+   let potatoes = 0;
+    while (counter > 0 && counter < 5) {
+       potatoes =+ counter;
+       document.getElementById("answer").innerHTML = potatoes + " potatoe, <br>";
+        counter++;
+    }
+    {
+       document.getElementById("answerContinued").innerHTML = "5 potatoe, 6 potatoe, 7 potatoe more...THE END!!!";
+ }
 }
