d14a03f51496ac7aca3a0d94fe14d7235384b789
LaurieSue <pink.bee.me@outlook.com>
2020-02-25 06:39:50 -0600

    added Array exercise and rearranged order ( put while loop at the end) Basic_HTML_10 w/ js file

--- JS/Basic_JavaScript_10.js
+++ JS/Basic_JavaScript_10.js
@@ -1,23 +1,3 @@
-function Potatoes_What() {
-var x= 1;
-var  count="";
-
-   
-    while (x < 7) {
-     count = x + " potatoe,<br>";
-     x++;
-     document.write(count);
-    } 
-    if (x=7){
-        document.write( x + " potatoe MORE!!!!<br>");
-    }
-    else if 
-        (x > 7); {document.write("The End");
-}
-}
-
-
-
 function for_Loop() {
 var Instruments = ["Tambourine", "Cello","Bass Viola","Flute", "Tuba", "Oboe", "Bass Guitar","Acoustic Guitar", "Electric Guitar", "Xylophone"];
 var Content = "";
@@ -30,3 +10,30 @@ var i;
   
 }
 
+function array_Function() {
+    var kiddos = [];
+    kiddos[0] = "Jordan Rebecca";
+    kiddos[1] = "Lawrence Niles";
+    kiddos[2] = "Frankie Paige";
+    kiddos[3] = "Dyson Gregory";
+    kiddos[4] = "Li'l Bit";
+    document.getElementById("Array").innerHTML = "I have four children:<br>" + kiddos[0] + ": 30<br>" + kiddos[1] + ": 23<br>" + kiddos[2] + ": 22<br>" + kiddos[3] + ": 20<br>" + "and the biggest kiddo of them all... My Staffy, " + kiddos[4] + ": 5";
+}
+
+function Potatoes_What() {
+    var x= 1;
+    var  count="";
+    
+       
+        while (x < 7) {
+         count = x + " potatoe,<br>";
+         x++;
+         document.write(count);
+        } 
+        if (x=7){
+            document.write( x + " potatoe MORE!!!!<br>");
+        }
+        else if 
+            (x > 7); {document.write("The End");
+    }
+    }
