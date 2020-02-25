c787639060d87f2630d704ab74332cf244c395f9
LaurieSue <pink.bee.me@outlook.com>
2020-02-24 19:26:34 -0600

    figured out potatoe issue in Basic_HTML_10.html and JS 10 file with Jordan's help

--- JS/Basic_JavaScript_10.js
+++ JS/Basic_JavaScript_10.js
@@ -2,9 +2,15 @@ function Potatoes_What() {
     var x= 1;
     count="";
    
-    while (x < 8) {
-     count =+ "<br>" + x;
-      x++;}{
-     document.write(count + " potatoes,");
+    while (x < 7) {
+     count = x + " potatoe,<br>";
+     x++;
+     document.write(count);
+    } 
+    if (x=7){
+        document.write( x + " potatoe MORE!!!!<br>");
+    }
+    else if 
+        (x > 7); {document.write("The End");
     }
 }
