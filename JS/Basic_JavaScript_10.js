53b627ca9c8b3bf000046223c333635897dea463
LaurieSue <pink.bee.me@outlook.com>
2020-02-26 22:40:12 -0600

    completed Let Keyword exercise on Basic_HTML_10 / Javascript_10 files and started HTML_Return / JS_Return Exercise

--- JS/Basic_JavaScript_10.js
+++ JS/Basic_JavaScript_10.js
@@ -1,3 +1,31 @@
+function constant_Function() {
+     const Instruments_Inv = {
+     type:"cello",
+     brand:"DZStrad",
+     color:"ebony"};
+ {document.getElementById("Constant").innerHTML = "Musical Instrument Selected and IN STOCK:<br>Instrument Type: " + Instruments_Inv.type +"<br>Brand: " + Instruments_Inv.brand + "<br>Color: " + Instruments_Inv.color;
+}
+}
+
+function constant_Update(){
+    const Instruments_Inv = {
+        type:"cello",
+        brand:"DZStrad",
+        color:"Ebony"};
+
+        Instruments_Inv.type ="piccolo";
+        Instruments_Inv.model = "#14917";
+        Instruments_Inv.brand ="Powell";
+        Instruments_Inv.price ="$19,190.00";
+        Instruments_Inv.color = "14K Rose Gold";
+
+    
+    {document.getElementById("Update").innerHTML ="Musical Instrument Selected and IN STOCK:<br>Instrument Type: " + Instruments_Inv.type +"<br>Brand: " + Instruments_Inv.brand + "<br>Model: " + Instruments_Inv.model + "<br>Price: " + Instruments_Inv.price +  "<br>Color: " + Instruments_Inv.color;
+   }
+}
+
+
+
 function for_Loop() {
 var Instruments = ["Tambourine", "Cello","Bass Viola","Flute", "Tuba", "Oboe", "Bass Guitar","Acoustic Guitar", "Electric Guitar", "Xylophone"];
 var Content = "";
@@ -26,14 +54,38 @@ function Potatoes_What() {
     
        
         while (x < 7) {
-         count = x + " potatoe,<br>";
-         x++;
-         document.write(count);
+         count += x + " potatoe,<br>";x++;
+        
+         document.getElementById("Potatoes").innerHTML = count;
         } 
         if (x=7){
-            document.write( x + " potatoe MORE!!!!<br>");
+            document.getElementById("Potatoes_More").innerHTML = x + " potatoe MORE!!!!<br>";
         }
         else if 
-            (x > 7); {document.write("The End");
+            (x > 7); {document.getElementById("Potatoes_Done").innerHTML = "The End";
     }
+    }
+
+    function Demo_Let_Keyword() {
+        document.getElementById("Example").innerHTML = 
+        `This is an example of BLOCK SCOPE using the LET variable, as shown through the Demo_Let_Keyword() function (below):<br><br>
+        
+        function Demo_Let_Keyword() {<br>
+            var x = 25;<br>
+            document.getElementById("Demo1").innerHTML = x;<br>
+            {<br>
+            let x = 25*2;<br>
+            document.getElementById("Demo2").innerHTML = x;<br>
+            }<br>
+            document.getElementById("Demo3").innerHTML = x;<br>
+            } <br><br>
+            This will Return the following:`;{
+
+        var x = 25;
+        document.getElementById("Demo1").innerHTML = x;}
+        {
+let x = 25*2;
+document.getElementById("Demo2").innerHTML = x + " (BLOCK SCOPE with the use of LET)";
+}
+document.getElementById("Demo3").innerHTML = x;
     }
