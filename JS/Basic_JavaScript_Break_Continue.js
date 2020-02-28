829cb37e675df47bf085e927e061a5a8ceba762d
LaurieSue <pink.bee.me@outlook.com>
2020-02-28 13:22:34 -0600

    continue statement not working because function is not defined?

--- JS/Basic_JavaScript_Break_Continue.js
+++ JS/Basic_JavaScript_Break_Continue.js
@@ -1,10 +1,10 @@
 function pickMe() {
-let count = 1;
+let count = "";
 let students = [Mary, Tom, Bob, Alex, Rick, Dave, Sue, Carl, Agnes, Gary, Pete, Bea, Bev, Gene, Jean, Lee, Leah, Tim, Burt, Will, Sal, Tia, Mya, Rudy];
 let nextStudentPick = function() {students[Math.floor(Math.random() * students.length)]};
 nextStudentPick=
 
-for (count < 8; studentPick != nextStudentPick
+while  (count = 1; count < 8; count++;) studentPick != nextStudentPick
     document.getElementById("You_Got_It").innerHTML = "Office Aid: " +  studentPick + "  Class Hour: " + count + "<br>" ;
  if (count === 8) {
            break;
@@ -12,3 +12,15 @@ for (count < 8; studentPick != nextStudentPick
 
 alert(`All Spots For Office Aid Are Full At This Time!
     Choose An ALternative Elective For This Semester.`);
+
+
+ function Not_Odd_At_All() {
+     for (var x = 1; x < 20; x++) {
+         if ((x % 2) != 0);
+        {
+         continue;
+     } else {
+   document.getElementById("No_OddBalls").innerHTML = "This beautiful " + x + ", is clearly Not 'Odd' at all!<br>";
+    }
+    }
+}
