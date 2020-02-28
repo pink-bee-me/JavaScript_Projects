591bbf23b3235221d54d03daf107eb2024d06908
LaurieSue <pink.bee.me@outlook.com>
2020-02-28 11:48:24 -0600

    trouble shoot pickMe() problem in my Break Demo HTML and JS files

---
+++ JS/Basic_JavaScript_Break_Continue.js
@@ -0,0 +1,14 @@
+function pickMe() {
+let count = 1;
+let students = [Mary, Tom, Bob, Alex, Rick, Dave, Sue, Carl, Agnes, Gary, Pete, Bea, Bev, Gene, Jean, Lee, Leah, Tim, Burt, Will, Sal, Tia, Mya, Rudy];
+let nextStudentPick = function() {students[Math.floor(Math.random() * students.length)]};
+nextStudentPick=
+
+for (count < 8; studentPick != nextStudentPick
+    document.getElementById("You_Got_It").innerHTML = "Office Aid: " +  studentPick + "  Class Hour: " + count + "<br>" ;
+ if (count === 8) {
+           break;
+ }
+
+alert(`All Spots For Office Aid Are Full At This Time!
+    Choose An ALternative Elective For This Semester.`);
