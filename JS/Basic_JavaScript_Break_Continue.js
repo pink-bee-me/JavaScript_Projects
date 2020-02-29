1105da06a341cde6244c12c75b0a0ec1d0d6093a
LaurieSue <pink.bee.me@outlook.com>
2020-02-29 16:09:29 -0600

    troubleshoot Break_Cointinue Assignment Step 199

--- JS/Basic_JavaScript_Break_Continue.js
+++ JS/Basic_JavaScript_Break_Continue.js
@@ -1,26 +1,26 @@
+let students= ["Mary","Tom","Bob","Alex","Rick","Dave","Sue","Carl","Agnes","Gary","Pete","Bea","Bev","Gene","Jean","Lee","Leah","Tim","Burt","Will","Sal","Tia","Mya","Rudy"];
+    let count = i + 1;
+    var pick ="";
 function pickMe() {
-let count = "";
-let students = [Mary, Tom, Bob, Alex, Rick, Dave, Sue, Carl, Agnes, Gary, Pete, Bea, Bev, Gene, Jean, Lee, Leah, Tim, Burt, Will, Sal, Tia, Mya, Rudy];
-let nextStudentPick = function() {students[Math.floor(Math.random() * students.length)]};
-nextStudentPick=
-
-while  (count = 1; count < 8; count++;) studentPick != nextStudentPick
-    document.getElementById("You_Got_It").innerHTML = "Office Aid: " +  studentPick + "  Class Hour: " + count + "<br>" ;
- if (count === 8) {
+   
+    for (i = 0; i < 10; i++)  {
+         pick[] = students[Math.floor(Math.random() * students.length)];
+    
+{    document.getElementById("You_Got_It").innerHTML = "Office Aid: " +  pick[i] + "  Class Hour: " + count + "<br>";}
+ if (count === 8); {
            break;
  }
-
+    }
 alert(`All Spots For Office Aid Are Full At This Time!
     Choose An ALternative Elective For This Semester.`);
 
 
+    var x;
  function Not_Odd_At_All() {
-     for (var x = 1; x < 20; x++) {
-         if ((x % 2) != 0);
-        {
-         continue;
-     } else {
-   document.getElementById("No_OddBalls").innerHTML = "This beautiful " + x + ", is clearly Not 'Odd' at all!<br>";
-    }
-    }
-}
+     for (x = 1; x < 20; x++) {
+         if ((x % 2) != 0){
+         continue;}
+         else{
+   document.getElementById("No_OddBalls").innerHTML = "This beautiful " + x + ", is clearly Not 'Odd' at all!<br>";}
+    document.getElementById("end").innerHTML = "The End."};
+     } }
