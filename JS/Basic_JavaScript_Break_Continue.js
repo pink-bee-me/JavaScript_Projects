86b06114f5bef35b4d5e61d01a12ed59c0a6a6ec
LaurieSue <pink.bee.me@outlook.com>
2020-03-01 15:55:55 -0600

    tweaked functions / reworked...still not quite right but did achieve some form of output

--- JS/Basic_JavaScript_Break_Continue.js
+++ JS/Basic_JavaScript_Break_Continue.js
@@ -1,26 +1,34 @@
-let students= ["Mary","Tom","Bob","Alex","Rick","Dave","Sue","Carl","Agnes","Gary","Pete","Bea","Bev","Gene","Jean","Lee","Leah","Tim","Burt","Will","Sal","Tia","Mya","Rudy"];
-    let count = i + 1;
-    var pick ="";
 function pickMe() {
-   
-    for (i = 0; i < 10; i++)  {
-         pick[] = students[Math.floor(Math.random() * students.length)];
-    
-{    document.getElementById("You_Got_It").innerHTML = "Office Aid: " +  pick[i] + "  Class Hour: " + count + "<br>";}
- if (count === 8); {
-           break;
- }
+
+
+const students=["Mary","Tom","Bob","Alex","Rick","Dave","Sue","Carl","Agnes","Gary","Pete","Bea","Bev","Gene","Jean","Lee","Leah","Tim","Burt","Will","Sal","Tia","Mya","Rudy"];
+    var i = 0;
+    var count = i + 1;
+    var pick ="";
+    const officeAid = [pick]
+    for ( i += i; i, i < students.length; i++)
+    { pick = students[Math.floor(Math.random() * students.length)];
+    officeAid.push(pick);
+
+        if (count === 8);{
+            break;
+        }
+    document.getElementById("You_Got_It").innerHTML = "Office Aid: " +  officeAid[i] + "  Class Hour: " + count + "<br>";
     }
-alert(`All Spots For Office Aid Are Full At This Time!
-    Choose An ALternative Elective For This Semester.`);
+    console.log(students.indexOf("Mary"));
+ 
+document.getElementById("Closed").innerHTML ='Student: ' + pick[i] + 'All Spots For Office Aid Are Full At This Time! Choose An ALternative Elective For This Semester.';
+}
 
 
-    var x;
+
+
  function Not_Odd_At_All() {
+     var x;
      for (x = 1; x < 20; x++) {
          if ((x % 2) != 0){
-         continue;}
-         else{
-   document.getElementById("No_OddBalls").innerHTML = "This beautiful " + x + ", is clearly Not 'Odd' at all!<br>";}
-    document.getElementById("end").innerHTML = "The End."};
-     } }
+              {continue;}
+              document.getElementById("No_OddBalls").innerHTML = "This beautiful " + x + ", is clearly Not 'Odd' at all!<br>";
+         }
+    document.getElementById("end").innerHTML = "The End.";
+         }}
