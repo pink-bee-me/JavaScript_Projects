3e10796ed5adaf65ea8226a3206d3982f1b5afdf
LaurieSue <pink.bee.me@outlook.com>
2020-03-02 01:45:39 -0600

    can NOT get my officeAid students to display one for each class period or my even numbers to list just the last number displays

--- JS/Basic_JavaScript_Break_Continue.js
+++ JS/Basic_JavaScript_Break_Continue.js
@@ -1,34 +1,51 @@
+const students=["Mary","Tom","Bob","Alex","Rick","Dave","Sue","Carl","Agnes","Gary","Pete","Bea","Bev","Gene","Jean","Lee","Leah","Tim","Burt","Will","Sal","Tia","Mya","Rudy"];
+    var i = 0;
+    var classPeriod = i +1;
+    
+    const officeAid = [];
+
+
 function pickMe() {
 
 
-const students=["Mary","Tom","Bob","Alex","Rick","Dave","Sue","Carl","Agnes","Gary","Pete","Bea","Bev","Gene","Jean","Lee","Leah","Tim","Burt","Will","Sal","Tia","Mya","Rudy"];
-    var i = 0;
-    var count = i + 1;
-    var pick ="";
-    const officeAid = [pick]
-    for ( i += i; i, i < students.length; i++)
-    { pick = students[Math.floor(Math.random() * students.length)];
-    officeAid.push(pick);
 
-        if (count === 8);{
-            break;
+
+    for (i < (students.length + 1); i++;)
+    {
+         let pick = students[Math.floor(Math.random() * students.length)];
+
+    officeAid.splice(0,0,pick);
+
+document.getElementById("You_Got_It").innerHTML = "Office Aid: " +  officeAid[i] + "  Class Hour: " + classPeriod + "<br>";
+
+
+          if (i > 6);{
+            break;}
+         
+          } 
+
+          document.getElementById("Closed").innerHTML = ' All Spots For Office Aid Are Full At This Time! Choose An ALternative Elective.';
         }
-    document.getElementById("You_Got_It").innerHTML = "Office Aid: " +  officeAid[i] + "  Class Hour: " + count + "<br>";
-    }
-    console.log(students.indexOf("Mary"));
- 
-document.getElementById("Closed").innerHTML ='Student: ' + pick[i] + 'All Spots For Office Aid Are Full At This Time! Choose An ALternative Elective For This Semester.';
-}
+
+
+
+
+
 
 
 
 
  function Not_Odd_At_All() {
-     var x;
-     for (x = 1; x < 20; x++) {
-         if ((x % 2) != 0){
-              {continue;}
-              document.getElementById("No_OddBalls").innerHTML = "This beautiful " + x + ", is clearly Not 'Odd' at all!<br>";
-         }
-    document.getElementById("end").innerHTML = "The End.";
-         }}
+     var x=1
+     for (x += x; x < 21;) { document.getElementById("No_OddBalls").innerHTML = "This beautiful " + x + ", is clearly Not 'Odd' at all!<br>";x++;
+    ((x % 2) != 0);{
+        continue;
+ (x > 21);
+    break;
+     }    
+
+     
+     
+     }  
+     document.getElementById("end").innerHTML = "The End.";
+    }
