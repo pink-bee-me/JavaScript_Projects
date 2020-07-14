5db25952ef25c20751ba8073c5db992ebc1be8e1
PinkBeeMe <51229658+PinkBeeMe@users.noreply.github.com>
2020-07-14 12:33:06 -0500

    AltBasic and Basic HTML_3 work

---
+++ JS/AltBasic_JavaScript_3.js
@@ -0,0 +1,58 @@
+function arithmeticCalc() {
+    var sum = 5 + 6;
+    document.getElementById("Math").innerHTML = "5 + 6 = " + sum;
+}
+
+
+function subtractionCalc() {
+    var subtraction = 21 - 11;
+    document.getElementById("Math_Subtraction").innerHTML = "21 - 11 = " + subtraction;
+}
+
+
+function multiplication() {
+    var simple_Math = 30 * 2;
+    document.getElementById("Multiply").innerHTML = "30 x 2 = " + simple_Math;
+}
+
+
+function division() {
+    var simple_Math = 30 / 2;
+    document.getElementById("Divide").innerHTML = "30 / 2 = " + simple_Math;
+}
+
+function multiple_Operation_Arithmetic() {
+    var simple_Math = (15 * 3) / (6 + 3) - 7;
+    document.getElementById("Multiple_Ops").innerHTML = "15 times 3, divided by the sum of 6 plus 3, then subtract 7... is equal to: " + simple_Math;
+}
+
+
+function modulus_Operation() {
+    var simple_Math = 99 % 5;
+    document.getElementById("Remainder").innerHTML = "The remainder, or modulus(%), of 99 divided by 5 is : " + simple_Math;
+    }
+
+
+function negation_Operator() {
+    var x = 11 * 2;
+    document.getElementById("Negation_Applied").innerHTML = -x;
+}
+
+
+function increment_PostOp() {
+    var x = 10;
+    x++;
+  document.getElementById("ValueUp").innerHTML = x;
+}
+
+function decrement_PostOp() {
+    var x = 10;
+    x--;
+   document.getElementById("ValueDown").innerHTML = x;
+}
+
+function random_Num() {
+    document.getElementById("Random_Num_Picker").innerHTML = window.alert(Math.floor(Math.random()*200);
+}
+
+
